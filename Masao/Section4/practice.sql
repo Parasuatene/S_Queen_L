edbeb98e1a333f60cb46e156f45fa21360f08971
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-30 14:36:21 +0900

    [add]練習問題4.1

---
+++ Masao/Section4/practice.sql
@@ -0,0 +1,23 @@
+--練習問題4.1
+--Aさんがここまで実行
+BEGIN TRANSACTION;
+INSERT INTO Shohin VALUES ('0001', 'Tシャツ' ,'衣服', 1000, 500, '2009-09-20');
+INSERT INTO Shohin VALUES ('0002', '穴あけパンチ', '事務用品', 500, 320, '2009-09-11');
+INSERT INTO Shohin VALUES ('0003', 'カッターシャツ', '衣服', 4000, 2800, NULL);
+--直後にBさんがこれを実行
+SELECT * FROM Shohin;
+/*
+【Aさん側で実行】
+ shohin_id |   shohin_mei   | shohin_bunrui | hanbai_tanka | shiire_tanka |  torokubi       
+-----------+----------------+---------------+--------------+--------------+------------     
+ 0001      | Tシャツ        | 衣服          |         1000 |          500 | 2009-09-20      
+ 0002      | 穴あけパンチ   | 事務用品      |          500 |          320 | 2009-09-11      
+ 0003      | カッターシャツ | 衣服          |         4000 |         2800 |
+(3 行)
+*/
+/*
+【Bさん側で実行】
+ shohin_id | shohin_mei | shohin_bunrui | hanbai_tanka | shiire_tanka | torokubi  
+-----------+------------+---------------+--------------+--------------+---------- 
+(0 行)
+*/
