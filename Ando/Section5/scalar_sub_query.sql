2cb714f3ff1353e48b58fbcc4d721e68d5efe4c7
Parasuatene <andoooo9.y.1127@gmail.com>
2022-05-30 16:22:07 +0900

    [add]スカラーサブクエリの実行

---
+++ Ando/Section5/scalar_sub_query.sql
@@ -0,0 +1,13 @@
+SELECT
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    (
+        SELECT
+            AVG(hanbai_tanka)
+        FROM
+            Shohin
+    ) AS avg_tanka
+FROM
+    Shohin
+;
