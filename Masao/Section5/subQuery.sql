17ded97e4e2f90a7cf4bf5700e3ee1926c4a8c98
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 10:18:31 +0900

    [update]List5-12:スカラ・サブクエリを活用

--- Masao/Section5/subQuery.sql
+++ Masao/Section5/subQuery.sql
@@ -33,3 +33,19 @@ SELECT shohin_bunrui, cnt_shohin --➂商品分類でグループ化されたテ
  キッチン用品  |          4
 (1 行)
 */
+
+--スカラ・サブクエリ（単一のデータを返すようなクエリ）
+SELECT shohin_id, shohin_mei, hanbai_tanka
+  FROM Shohin
+ WHERE hanbai_tanka > (
+     SELECT AVG(hanbai_tanka)
+       FROM Shohin
+ );
+/*
+ shohin_id |   shohin_mei   | hanbai_tanka
+-----------+----------------+--------------
+ 0003      | カッターシャツ |         4000
+ 0004      | 包丁           |         3000
+ 0005      | 圧力鍋         |         6800
+(3 行)
+*/
