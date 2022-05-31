1d9c1d1868225e2f3f0cbcc93efb6d16b0e78cb8
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 13:13:51 +0900

    [update]List6-18:文字列型から日付型へ変換

--- Masao/Section6/alterFunc.sql
+++ Masao/Section6/alterFunc.sql
@@ -6,3 +6,12 @@ SELECT CAST('0001' AS INTEGER) AS int_col;
        1
 (1 行)
 */
+
+--文字列型から日付型へ変換
+SELECT CAST('2009-12-14' AS DATE) AS date_col;
+/*
+  date_col
+------------
+ 2009-12-14
+(1 行)
+*/
