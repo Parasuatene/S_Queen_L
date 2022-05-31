1dd3b5a459d18cc1f0422a73c2c1ce8c793fccf4
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 22:39:34 +0900

    [update]List7-10:内部結合とWHEREを組み合わせる

--- Masao/Section7/join.sql
+++ Masao/Section7/join.sql
@@ -23,3 +23,22 @@ SELECT TS.tenpo_id,
  000C     | 大阪      | 0006      | フォーク       |          500
  000C     | 大阪      | 0007      | おろしがね     |          880
 */
+
+--内部結合とWHEREを組み合わせる
+SELECT TS.tenpo_id, 
+       TS.tenpo_mei, 
+       TS.shohin_id, 
+       S.shohin_mei, 
+       S.hanbai_tanka
+  FROM TenpoShohin AS TS
+  JOIN Shohin      AS S 
+    ON TS.shohin_id = S.shohin_id
+ WHERE TS.tenpo_id = '000A';
+/*
+ tenpo_id | tenpo_mei | shohin_id |   shohin_mei   | hanbai_tanka 
+----------+-----------+-----------+----------------+--------------
+ 000A     | 東京      | 0001      | Tシャツ        |         1000
+ 000A     | 東京      | 0002      | 穴あけパンチ   |          500
+ 000A     | 東京      | 0003      | カッターシャツ |         4000
+(3 rows)
+*/
