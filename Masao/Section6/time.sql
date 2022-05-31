5446628efb7a29060b2f4db43f2f4d227bf0ca05
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 13:08:41 +0900

    [update]List6-16:EXTRACT関数で日付の切り出し

--- Masao/Section6/time.sql
+++ Masao/Section6/time.sql
@@ -24,3 +24,18 @@ SELECT CURRENT_TIMESTAMP;
  2022-05-31 11:53:45.143144+09
 (1 行)
 */
+
+--日付要素の切り出し
+SELECT CURRENT_TIMESTAMP,
+       EXTRACT(YEAR   FROM CURRENT_TIMESTAMP) AS year,
+       EXTRACT(MONTH  FROM CURRENT_TIMESTAMP) AS month,
+       EXTRACT(DAY    FROM CURRENT_TIMESTAMP) AS day,
+       EXTRACT(HOUR   FROM CURRENT_TIMESTAMP) AS hour,
+       EXTRACT(MINUTE FROM CURRENT_TIMESTAMP) AS mitute,
+       EXTRACT(SECOND FROM CURRENT_TIMESTAMP) AS second;
+/*
+              now              | year | month | day | hour | mitute |  second
+-------------------------------+------+-------+-----+------+--------+-----------
+ 2022-05-31 13:07:51.706766+09 | 2022 |     5 |  31 |   13 |      7 | 51.706766
+(1 行)
+*/
