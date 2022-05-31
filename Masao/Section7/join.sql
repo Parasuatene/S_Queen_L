9366486d526d01c8ad9b100142a2a1bd7be0ca7e
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 22:58:23 +0900

    [update]List7-14:3つのテーブルを内部結合する

--- Masao/Section7/join.sql
+++ Masao/Section7/join.sql
@@ -72,3 +72,34 @@ SELECT TS.tenpo_id,
           |           |           | 圧力鍋         |         6800
 (15 rows)
 */
+
+--3つのテーブルを内部結合
+SELECT TS.tenpo_id, 
+       TS.tenpo_mei, 
+       TS.shohin_id, 
+       S.shohin_mei, 
+       S.hanbai_tanka
+  FROM TenpoShohin  AS TS
+  INNER JOIN Shohin AS S
+    ON TS.shohin_id = S.shohin_id
+        INNER JOIN ZaikoShohin AS ZS
+        ON TS.shohin_id = ZS.shohin_id
+    WHERE ZS.souko_id = 'S001';
+/*
+ tenpo_id | tenpo_mei | shohin_id |   shohin_mei   | hanbai_tanka 
+----------+-----------+-----------+----------------+--------------
+ 000A     | 東京      | 0001      | Tシャツ        |         1000
+ 000A     | 東京      | 0002      | 穴あけパンチ   |          500
+ 000A     | 東京      | 0003      | カッターシャツ |         4000
+ 000B     | 名古屋    | 0002      | 穴あけパンチ   |          500
+ 000B     | 名古屋    | 0003      | カッターシャツ |         4000
+ 000B     | 名古屋    | 0004      | 包丁           |         3000
+ 000B     | 名古屋    | 0006      | フォーク       |          500
+ 000B     | 名古屋    | 0007      | おろしがね     |          880
+ 000C     | 大阪      | 0003      | カッターシャツ |         4000
+ 000C     | 大阪      | 0004      | 包丁           |         3000
+ 000C     | 大阪      | 0006      | フォーク       |          500
+ 000C     | 大阪      | 0007      | おろしがね     |          880
+ 000D     | 福岡      | 0001      | Tシャツ        |         1000
+(13 rows)
+*/
