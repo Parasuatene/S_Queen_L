dc9cdfc45b701d58f3c13b1c44b8ab814cf4042c
Parasuatene <andoooo9.y.1127@gmail.com>
2022-05-31 13:48:58 +0900

    [add]追加課題1-4

---
+++ Ando/AddPractice1/problem_4.sql
@@ -0,0 +1,12 @@
+SELECT
+    last_name,
+    job_id,
+    to_char(hire_date, 'DD-Mon-MM')  -- 日付を指定したフォーマットに変換する
+FROM
+    employees
+WHERE
+    last_name = 'Matos'
+    OR last_name = 'Taylor'
+ORDER BY
+    hire_date ASC
+;
