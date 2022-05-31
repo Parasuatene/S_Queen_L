1f16b45faee52dc092f917bc4b7ed9e6c25548a1
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 11:33:31 +0900

    [update]List6-8:LENGTH関数で文字列の長さを求める

--- Masao/Section6/string.sql
+++ Masao/Section6/string.sql
@@ -18,3 +18,24 @@ SELECT str1, str2,
  ミックマック | ッ     | ミックマックッ
 (11 行)
 */
+
+--文字列の長さを調べる
+SELECT str1,
+       LENGTH(str1) AS len_str
+  FROM SampleStr;
+/*
+     str1     | len_str
+--------------+---------
+ あいう       |       3
+ abc          |       3
+ 山田         |       2
+ aaa          |       3
+              |
+ @!#$%        |       5
+ ABC          |       3
+ aBC          |       3
+ abc太郎      |       5
+ abcdefabc    |       9
+ ミックマック |       6
+(11 行)
+*/
