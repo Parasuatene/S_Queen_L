ac638b8fbf53c370fbf67b8c92fe7f3104ed9b0f
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 10:56:13 +0900

    [update]練習問題5.2

--- Masao/Section5/practice.sql
+++ Masao/Section5/practice.sql
@@ -14,3 +14,10 @@ SELECT * FROM ViewRenshu5_1;
  包丁       |         3000 | 2009-09-20
 (2 行)
 */
+
+--練習問題5.2
+INSERT INTO ViewRenshu5_1 VALUES ('ナイフ', 300, '2009-11-02'); --INSERT指定できていない要素があるため実行できない（今回はidがnot null制約があって引っかかる）
+/*
+ERROR:  列"shohin_id"内のNULL値はNOT NULL制約違反です
+DETAIL:  失敗した行は(null, ナイフ, null, 300, null, 2009-11-02)を含みます
+*/
