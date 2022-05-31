ac218bc9962b30d608da6269b6f83a1e37871aef
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 15:24:50 +0900

    [update]List6-43:CASE式を使った行列変換

--- Masao/Section6/case.sql
+++ Masao/Section6/case.sql
@@ -22,3 +22,30 @@ SELECT shohin_mei,
  ボールペン     | B:事務用品
 (8 行)
 */
+
+--CASE式を使って行列変換
+SELECT SUM(
+            CASE WHEN shohin_bunrui = '衣服'
+                 THEN hanbai_tanka
+                 ELSE 0
+            END
+        ) AS sum_tanka_ihuku,
+        SUM(
+            CASE WHEN shohin_bunrui = 'キッチン用品'
+                 THEN hanbai_tanka
+                 ELSE 0
+            END
+        ) AS sum_tanka_kitchen,
+        SUM(
+            CASE WHEN shohin_bunrui = '事務用品'
+                 THEN hanbai_tanka
+                 ELSE 0
+            END
+        ) AS sum_tanka_jimu
+  FROM Shohin;
+/*
+ sum_tanka_ihuku | sum_tanka_kitchen | sum_tanka_jimu
+-----------------+-------------------+----------------
+            5000 |             11180 |            600
+(1 行)
+*/
