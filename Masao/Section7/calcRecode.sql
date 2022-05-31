5b9a12538eec5d5d63daffb9f5b7d9b6d201e836
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 16:19:45 +0900

    [update]List7-4:UNION使用時のORDER BYの仕様を確認

--- Masao/Section7/calcRecode.sql
+++ Masao/Section7/calcRecode.sql
@@ -19,3 +19,23 @@ SELECT shohin_id, shohin_mei
  0005      | 圧力鍋
 (10 行)
 */
+
+--UNIONを使用した場合のORDER BYは最後に一つだけ
+ SELECT shohin_id, shohin_mei
+   FROM Shohin
+  WHERE shohin_bunrui = 'キッチン用品'
+  UNION
+ SELECT shohin_id, shohin_mei
+   FROM Shohin2
+  WHERE shohin_bunrui = 'キッチン用品'
+ORDER BY shohin_id; --ここに一回だけ書く
+/*
+ shohin_id | shohin_mei
+-----------+------------
+ 0004      | 包丁
+ 0005      | 圧力鍋
+ 0006      | フォーク
+ 0007      | おろしがね
+ 0010      | やかん
+(5 行)
+*/
