337ab1040f452025d43a2003d7b6126b4bdc1d2f
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 23:22:38 +0900

    [update]練習問題7.2

--- Masao/Section7/practice.sql
+++ Masao/Section7/practice.sql
@@ -21,3 +21,33 @@ INTERSECT
  0008      | ボールペン     | 事務用品      |          100 |              | 2009-11-11
 (8 rows)
 */
+
+--練習問題7.2
+SELECT COALESCE(TS.tenpo_id, '不明') AS tenpo_id, 
+       COALESCE(TS.tenpo_mei, '不明') AS tenpo_mei, 
+       TS.shohin_id, 
+       S.shohin_mei, 
+       S.hanbai_tanka
+  FROM TenpoShohin  AS TS
+  RIGHT OUTER JOIN Shohin AS S
+    ON TS.shohin_id = S.shohin_id;
+/*
+ tenpo_id | tenpo_mei | shohin_id |   shohin_mei   | hanbai_tanka 
+----------+-----------+-----------+----------------+--------------
+ 000A     | 東京      | 0001      | Tシャツ        |         1000
+ 000A     | 東京      | 0002      | 穴あけパンチ   |          500
+ 000A     | 東京      | 0003      | カッターシャツ |         4000
+ 000B     | 名古屋    | 0002      | 穴あけパンチ   |          500
+ 000B     | 名古屋    | 0003      | カッターシャツ |         4000
+ 000B     | 名古屋    | 0004      | 包丁           |         3000
+ 000B     | 名古屋    | 0006      | フォーク       |          500
+ 000B     | 名古屋    | 0007      | おろしがね     |          880
+ 000C     | 大阪      | 0003      | カッターシャツ |         4000
+ 000C     | 大阪      | 0004      | 包丁           |         3000
+ 000C     | 大阪      | 0006      | フォーク       |          500
+ 000C     | 大阪      | 0007      | おろしがね     |          880
+ 000D     | 福岡      | 0001      | Tシャツ        |         1000
+ 不明     | 不明      |           | ボールペン     |          100
+ 不明     | 不明      |           | 圧力鍋         |         6800
+(15 rows)
+*/
