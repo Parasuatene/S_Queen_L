26c41ed35053703364f6fe4f447164fe988c4cd2
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 11:30:44 +0900

    [add]List6-6:||で文字列を連結

---
+++ Masao/Section6/string.sql
@@ -0,0 +1,20 @@
+--文字列の連結
+SELECT str1, str2,
+       str1 || str2 AS str_concat
+  FROM SampleStr;
+/*
+     str1     |  str2  |   str_concat
+--------------+--------+----------------
+ あいう       | えお   | あいうえお
+ abc          | def    | abcdef
+ 山田         | 太郎   | 山田太郎
+ aaa          |        |
+              | あああ |
+ @!#$%        |        |
+ ABC          |        |
+ aBC          |        |
+ abc太郎      | abc    | abc太郎abc
+ abcdefabc    | abc    | abcdefabcabc
+ ミックマック | ッ     | ミックマックッ
+(11 行)
+*/
