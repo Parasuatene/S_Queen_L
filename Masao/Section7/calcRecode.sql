a86a591ffedf4e428a8f949ec7607aeb74b9bcb2
masaoguchi <masaoguchi.redring@gmail.com>
2022-05-31 16:21:53 +0900

    [update]List7-5:UNION使用時にALLをつけることで重複を許可する

--- Masao/Section7/calcRecode.sql
+++ Masao/Section7/calcRecode.sql
@@ -39,3 +39,28 @@ ORDER BY shohin_id; --ここに一回だけ書く
  0010      | やかん
 (5 行)
 */
+
+--UNIONで重複を残す
+SELECT shohin_id, shohin_mei
+  FROM Shohin
+ UNION ALL --ALLをつける
+SELECT shohin_id, shohin_mei
+  FROM Shohin2;
+/*
+ shohin_id |   shohin_mei
+-----------+----------------
+ 0001      | Tシャツ
+ 0002      | 穴あけパンチ
+ 0003      | カッターシャツ
+ 0004      | 包丁
+ 0005      | 圧力鍋
+ 0006      | フォーク
+ 0007      | おろしがね
+ 0008      | ボールペン
+ 0001      | Tシャツ
+ 0002      | 穴あけパンチ
+ 0003      | カッターシャツ
+ 0009      | 手袋
+ 0010      | やかん
+(13 行)
+*/
