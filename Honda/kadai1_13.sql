d4c69a5b32d285ca6614bf40c5485360244fa26d
Honda <hondahiroyuki0715@gmail.com>
2022-06-05 12:15:10 +0900

    [add]kadai2_1.sql(max,min,sum,averageの表示)

--- Honda/kadai1_13.sql
+++ Honda/kadai1_13.sql
@@ -1,5 +1,9 @@
 -- 姓の中にaとeの両方が含まれる全ての人の姓を全て表示する
 SELECT last_name
 FROM employees
-WHERE last_name LIKE '%a%e%'
-OR last_name LIKE '%e%a%';
+WHERE last_name SIMILAR TO ('%a%e%|%e%a%');
+
+-- WHERE last_name LIKE '%a%e%'
+-- OR last_name LIKE '%e%a%';
+
+-- WHERE last_name LIKE ('%a%e%|%e%a%');
